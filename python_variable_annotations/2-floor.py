ff836c95473d554ce668b75d7a7a0f82522c7325
GG-Muniz <g.garciamuniz@outlook.com>
2025-07-30 18:11:24 -0400

    floor - basic annotations - takes n as argument and returns its floor value.

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for floor function with type annotations."""
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float.
+    
+    Args:
+        n: Float number to floor
+        
+    Returns:
+        Floor of n as an integer
+    """
+    return math.floor(n)
