ec72819309e5c687031c4d273675f3080e5d498f
GG-Muniz <g.garciamuniz@outlook.com>
2025-07-31 12:46:05 -0400

    fixing whitespaces adn new line at eof - pycodestyle.

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -10,10 +10,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Create and return an asyncio.Task for wait_random.
-    
+
     Args:
         max_delay (int): Maximum delay for wait_random
-        
+
     Returns:
         asyncio.Task: A task object for the wait_random coroutine
     """
