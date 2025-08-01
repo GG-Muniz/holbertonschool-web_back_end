5a62afb91aabe436f671231f4ad6d9d4bd048fff
GG-Muniz <g.garciamuniz@outlook.com>
2025-08-01 19:59:23 -0400

    fixing asyncio.gather use.

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -18,11 +18,6 @@ async def measure_runtime() -> float:
         float: Total runtime in seconds
     """
     start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
     end_time = time.time()
     return end_time - start_time
