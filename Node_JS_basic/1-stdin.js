f8749251dea11acbff6388fbcaf3da2d0d5db099
GG-Muniz <g.garciamuniz@outlook.com>
2025-09-05 17:47:52 -0400

    Program uses stdin -> display a welcome message asking for user input.

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    const input = chunk.toString().trim();
+    if (input.length > 0) {
+      process.stdout.write(`Your name is: ${input}\n`);
+    }
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
